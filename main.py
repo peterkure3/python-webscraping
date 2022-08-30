6fb8878973a0e3d3030a5413ef3be5ea8d4052b2
Peter Kure <peterkure256@gmail.com>
2022-08-30 20:47:13 +0300

    Web scraper tutorial code

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+page_to_scrape = requests.get('https://quotes.toscrape.com/')
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
+quotes = soup.findAll("span", attrs={"class":"author"})
+authors = soup.findAll("small", attrs={"class":"author"})
+
+file = open("scraped_quotes.csv","w")
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES", "AUTHORS"])
+
+
+for quote, author in zip(quotes, authors):
+    print(quote.text + " - " + author.text)
+    writer.writerow([quote.text, author.text])
+file.close()
